5176787b996b42aa749dd727b76085b041bcc4e2
Natale-Epita-Java <natale1.mamberti@epita.fr>
2022-03-17 16:03:42 +0100

    fullLogger

--- src/main/java/fr/lernejo/logger/LoggerFactory.java
+++ src/main/java/fr/lernejo/logger/LoggerFactory.java
@@ -2,15 +2,8 @@ package fr.lernejo.logger;
 
 public class LoggerFactory {
     public static Logger getLogger(String name) {
-        Logger log = new ContextualLogger(
-            name,
-            new CompositeLogger(
-                new ConsoleLogger(),
-                new FilteredLogger(
-                    new FileLogger("output.txt"),
-                    message -> message.contains("Simulation")
-                )
-            )
+        Logger log = new ContextualLogger(name,
+            new CompositeLogger(new ConsoleLogger(), new FilteredLogger(new FileLogger("output.txt"), message -> message.contains("Simulation")))
         );
         return log;
     }
